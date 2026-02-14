e27566d9fc3fcaf28f66cee933ef2ee1bbe3a411
Farrukh Khamrakulov <freddiefarruh@gmail.com>
2026-02-15 04:29:56 +0500

    feat: initialize firebase for push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:passenger/app/app.dart';
 import 'package:passenger/app/di/injection.dart';
 import 'package:passenger/core/network/connectivity_service.dart';
+import 'package:passenger/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
